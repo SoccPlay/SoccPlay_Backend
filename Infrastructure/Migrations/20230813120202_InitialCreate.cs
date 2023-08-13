14f3161beb4e3521951da890151060bab5cc6cdc
dotis-flasnhing <xuanduy650@gmail.com>
2023-08-13 19:40:02 +0700

    Fix Data

---
+++ Infrastructure/Migrations/20230813120202_InitialCreate.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    AccountID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Password = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Role = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.AccountID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    AdminId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Address = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Email = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Status = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    AccountID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.AdminId);
+                    table.ForeignKey(
+                        name: "FKAdmin327316",
+                        column: x => x.AccountID,
+                        principalTable: "Account",
+                        principalColumn: "AccountID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Address = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Email = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Status = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    AccountID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerId);
+                    table.ForeignKey(
+                        name: "FKCustomer31171",
+                        column: x => x.AccountID,
+                        principalTable: "Account",
+                        principalColumn: "AccountID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owner",
+                columns: table => new
+                {
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Address = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Email = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Status = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    AccountID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owner", x => x.OwnerId);
+                    table.ForeignKey(
+                        name: "FKOwner823493",
+                        column: x => x.AccountID,
+                        principalTable: "Account",
+                        principalColumn: "AccountID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Booking",
+                columns: table => new
+                {
+                    BookingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TotalPrice = table.Column<float>(type: "real", nullable: false),
+                    DateBooking = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Note = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Booking", x => x.BookingId);
+                    table.ForeignKey(
+                        name: "FKBooking249093",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Land",
+                columns: table => new
+                {
+                    LandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameLand = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Title = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Policy = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Location = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    TotalPitch = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Land", x => x.LandId);
+                    table.ForeignKey(
+                        name: "FKLand822092",
+                        column: x => x.OwnerId,
+                        principalTable: "Owner",
+                        principalColumn: "OwnerId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    FeedbackId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Rate = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    LandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.FeedbackId);
+                    table.ForeignKey(
+                        name: "FKFeedback523885",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId");
+                    table.ForeignKey(
+                        name: "FKFeedback81330",
+                        column: x => x.LandId,
+                        principalTable: "Land",
+                        principalColumn: "LandId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PitchImage",
+                columns: table => new
+                {
+                    ImageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    LandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FKPitchImage851248",
+                        column: x => x.LandId,
+                        principalTable: "Land",
+                        principalColumn: "LandId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Price",
+                columns: table => new
+                {
+                    PriceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StarTime = table.Column<int>(type: "int", nullable: false),
+                    EndTime = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    LandLandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Price", x => x.PriceId);
+                    table.ForeignKey(
+                        name: "FKPrice403220",
+                        column: x => x.LandLandId,
+                        principalTable: "Land",
+                        principalColumn: "LandId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pitch",
+                columns: table => new
+                {
+                    PitchId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    Size = table.Column<int>(type: "int", unicode: false, nullable: false),
+                    Status = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    LandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PriceId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pitch", x => x.PitchId);
+                    table.ForeignKey(
+                        name: "FK_Pitch_Price_PriceId",
+                        column: x => x.PriceId,
+                        principalTable: "Price",
+                        principalColumn: "PriceId");
+                    table.ForeignKey(
+                        name: "FKPitch585708",
+                        column: x => x.OwnerId,
+                        principalTable: "Owner",
+                        principalColumn: "OwnerId");
+                    table.ForeignKey(
+                        name: "FKPitch63225",
+                        column: x => x.LandId,
+                        principalTable: "Land",
+                        principalColumn: "LandId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    ScheduleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StarTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Status = table.Column<string>(type: "varchar(max)", unicode: false, maxLength: 2147483646, nullable: false),
+                    PitchPitchId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookingBookingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.ScheduleId);
+                    table.ForeignKey(
+                        name: "FKSchedule603514",
+                        column: x => x.BookingBookingId,
+                        principalTable: "Booking",
+                        principalColumn: "BookingId");
+                    table.ForeignKey(
+                        name: "FKSchedule967594",
+                        column: x => x.PitchPitchId,
+                        principalTable: "Pitch",
+                        principalColumn: "PitchId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admin_AccountID",
+                table: "Admin",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Booking_CustomerId",
+                table: "Booking",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_AccountID",
+                table: "Customer",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_CustomerId",
+                table: "Feedback",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_LandId",
+                table: "Feedback",
+                column: "LandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Land_OwnerId",
+                table: "Land",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owner_AccountID",
+                table: "Owner",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pitch_LandId",
+                table: "Pitch",
+                column: "LandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pitch_OwnerId",
+                table: "Pitch",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pitch_PriceId",
+                table: "Pitch",
+                column: "PriceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PitchImage_LandId",
+                table: "PitchImage",
+                column: "LandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Price_LandLandId",
+                table: "Price",
+                column: "LandLandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_BookingBookingId",
+                table: "Schedule",
+                column: "BookingBookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_PitchPitchId",
+                table: "Schedule",
+                column: "PitchPitchId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Feedback");
+
+            migrationBuilder.DropTable(
+                name: "PitchImage");
+
+            migrationBuilder.DropTable(
+                name: "Schedule");
+
+            migrationBuilder.DropTable(
+                name: "Booking");
+
+            migrationBuilder.DropTable(
+                name: "Pitch");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Price");
+
+            migrationBuilder.DropTable(
+                name: "Land");
+
+            migrationBuilder.DropTable(
+                name: "Owner");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
