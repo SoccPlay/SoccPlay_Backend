d4432e137b9e2a2f9759d7542d11eddaed2d05ce
NhaPhan06 <phanthanhnha06@gmail.com>
2023-08-15 09:53:25 +0700

    Fix show image

--- Infrastructure/RepositoryImp/PitchImageRepository.cs
+++ Infrastructure/RepositoryImp/PitchImageRepository.cs
@@ -24,7 +24,7 @@ public class PitchImageRepository : GenericRepository<PitchImage>, IPitchImageRe
 
     public async Task<string> GetImageByLandId(Guid LandId)
     {
-        var list = _context.Set<PitchImage>().OrderByDescending(item => item.LandId).FirstOrDefault();
+        var list = _context.Set<PitchImage>().Where(land => land.LandId == LandId).OrderByDescending(item => item.LandId).FirstOrDefault();
         if (list == null)
         {
             return null;
