b90c06da04bdb1c8c5669adc7f9111ba4d7e8fe3
NhaPhan06 <phanthanhnha06@gmail.com>
2023-08-22 08:52:03 +0700

    Calculor

---
+++ Application/Model/Request/RequestPrice/RequestCaculator.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Model.Request.RequestPrice;
+
+public class RequestCaculator
+{
+    [Required] public Guid LandId { get; set; }
+    
+    [Required] public int Size { get; set; }
+    
+    [Required] public DateTime StarTime { get; set; }
+
+    [Required] public DateTime EndTime { get; set; }
+}
