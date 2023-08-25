0d3227dd4895bdc946e52c1193d4817f2e81e407
NhaPhan06 <phanthanhnha06@gmail.com>
2023-08-25 08:41:17 +0700

    update

--- Application/Model/Response/ResponseBooking/ResponseBooking.cs
+++ Application/Model/Response/ResponseBooking/ResponseBooking.cs
@@ -4,6 +4,7 @@ public class ResponseBooking
 {
     public Guid BookingId { get; set; }
     public float TotalPrice { get; set; }
+    public Guid LandId { get; set; }
     public string? Location { get; set; }
     public DateTime DateBooking { get; set; }
     public string Note { get; set; } = null!;
